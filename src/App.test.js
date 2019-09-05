2b62b83a2e003142d7081115ff9032f661ce52a2
Shiv Parikh <parikhshiv@google.com>
2019-09-05 16:41:16 -0700

    add enzyme and simple test for side nav

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { App } from './App';
+import Header from './header/Header';
+import SideNav from './sidenav/SideNav';
+import { shallow } from 'enzyme';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
+});
+
+it('renders a header', () => {
+  const wrapper = shallow(<App />);
+  expect(wrapper.contains(<Header/>)).toEqual(true);
+});
+
+it('renders a side nav', () => {
+  const wrapper = shallow(<App />);
+  expect(wrapper.contains(<SideNav/>)).toEqual(true);
 });
--- src/sidenav/SideNav.test.js
+++ src/sidenav/SideNav.test.js
@@ -1,10 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import SideNav from './SideNav';
+import MenuItem from '@material-ui/core/MenuItem';
+import Paper from '@material-ui/core/Paper';
+import { ROUTES } from '../App';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { mount } from 'enzyme';
 
 it('renders without crashing (inside of a Router)', () => {
   const div = document.createElement('div');
   ReactDOM.render(<Router><SideNav /></Router>, div);
   ReactDOM.unmountComponentAtNode(div);
+});
+
+it('renders a <MenuItem/> nav link for each of the routes specified in App.js',
+  () => {
+    const sideNavWrapper = mount(<Router><SideNav /></Router>);
+
+    const numRoutes = Object.keys(ROUTES).length;
+    expect(sideNavWrapper.find('li.side-nav-menu-item').length)
+      .toEqual(numRoutes);
 });
