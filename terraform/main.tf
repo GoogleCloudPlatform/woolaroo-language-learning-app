13293636e8023b6108b6fd5b0a13335a2b7cbc05
Aaron Neugebauer <aaron.n@rushdigital.co.nz>
2019-09-11 17:39:31 +1200

    Started adding load balancer to terraform script.

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,11 +1,12 @@
 variable "google_project" {}
+variable "google_region" {}
 variable "bucket_name" {}
 variable "bucket_location" {
-	default = ""
+  default = ""
 }
 
 output "bucket_url" {
-	value = "https://storage.googleapis.com/${var.bucket_name}/index.html"
+  value = "https://storage.googleapis.com/${var.bucket_name}/index.html"
 }
 
 provider "google" {
@@ -29,4 +30,29 @@ resource "google_storage_bucket_iam_member" "app-store-acl" {
   bucket = "${google_storage_bucket.app-store.name}"
   role = "roles/storage.objectViewer"
   member = "allUsers"
+}
+
+resource "google_compute_backend_bucket" "app-backend" {
+  name        = "${var.bucket_name}-backend"
+  bucket_name = "${google_storage_bucket.app-store.name}"
+  enable_cdn  = true
+}
+
+resource "google_compute_global_address" "app-ip" {
+  name = "${var.bucket_name}-ip"
+}
+
+resource "google_compute_forwarding_rule" "app-forwarding-rule" {
+  name                  = "${var.bucket_name}-forwarding-rule"
+  load_balancing_scheme = "EXTERNAL"
+  backend_service       = "${google_compute_backend_bucket.app-backend.self_link}"
+  port_range            = "80"
+  ip_address            = "${google_compute_global_address.app-ip.address}"
+}
+
+resource "google_compute_route" "app-route" {
+  name         = "${var.bucket_name}-route"
+  dest_range   = "0.0.0.0/0"
+  next_hop_ilb = "${google_compute_forwarding_rule.app-forwarding-rule.self_link}"
+  priority     = 2000
 }
