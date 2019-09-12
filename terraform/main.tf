8e6d525ca1b724170ec740cb7c3eaa27713bd8f3
Aaron Neugebauer <aaron.n@rushdigital.co.nz>
2019-09-12 22:55:47 +1200

    Completed basic terraform script.

--- terraform/main.tf
+++ terraform/main.tf
@@ -6,23 +6,24 @@ variable "bucket_location" {
 }
 
 output "bucket_url" {
-  value = "https://storage.googleapis.com/${var.bucket_name}/index.html"
+  value = "http://${google_compute_global_address.app-ip.address}/index.html"
 }
 
 provider "google" {
   credentials = "${file("account.json")}"
-  project     = var.google_project
+  project = var.google_project
+  region = var.google_region
 }
 
 resource "google_storage_bucket" "app-store" {
-  name     = var.bucket_name
+  name = var.bucket_name
   location = var.bucket_location
   storage_class = "MULTI_REGIONAL"
   bucket_policy_only = true
 
   website {
     main_page_suffix = "index.html"
-    not_found_page   = "404.html"
+    not_found_page = "404.html"
   }
 }
 
@@ -33,26 +34,28 @@ resource "google_storage_bucket_iam_member" "app-store-acl" {
 }
 
 resource "google_compute_backend_bucket" "app-backend" {
-  name        = "${var.bucket_name}-backend"
+  name = "${var.bucket_name}-backend"
   bucket_name = "${google_storage_bucket.app-store.name}"
-  enable_cdn  = true
+  enable_cdn = true
 }
 
 resource "google_compute_global_address" "app-ip" {
   name = "${var.bucket_name}-ip"
 }
 
-resource "google_compute_forwarding_rule" "app-forwarding-rule" {
-  name                  = "${var.bucket_name}-forwarding-rule"
-  load_balancing_scheme = "EXTERNAL"
-  backend_service       = "${google_compute_backend_bucket.app-backend.self_link}"
-  port_range            = "80"
-  ip_address            = "${google_compute_global_address.app-ip.address}"
+resource "google_compute_global_forwarding_rule" "app-forwarding-rule" {
+  name = "${var.bucket_name}-forwarding-rule"
+  port_range = "80"
+  ip_address = "${google_compute_global_address.app-ip.address}"
+  target = "${google_compute_target_http_proxy.app-http-proxy.self_link}"
 }
 
-resource "google_compute_route" "app-route" {
-  name         = "${var.bucket_name}-route"
-  dest_range   = "0.0.0.0/0"
-  next_hop_ilb = "${google_compute_forwarding_rule.app-forwarding-rule.self_link}"
-  priority     = 2000
+resource "google_compute_target_http_proxy" "app-http-proxy" {
+  name = "${var.bucket_name}-http-proxy"
+  url_map = "${google_compute_url_map.app-url-map.self_link}"
+}
+
+resource "google_compute_url_map" "app-url-map" {
+  name = "${var.bucket_name}-url-map"
+  default_service = "${google_compute_backend_bucket.app-backend.self_link}"
 }
