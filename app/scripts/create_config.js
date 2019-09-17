229e4852725fc29c3c2fd8984f85f97588bae497
Aaron Neugebauer <aaron.n@rushdigital.co.nz>
2019-09-18 11:15:12 +1200

    Added cloud build configuration.

---
+++ app/scripts/create_config.js
@@ -0,0 +1,18 @@
+const fs = require('fs');
+const path = require('path');
+
+const GOOGLE_API_KEY = process.argv[2];
+if(!GOOGLE_API_KEY) {
+    throw new Error('Google API key not set');
+}
+
+const configContent =
+`import { mergeConfigurations } from 'util/config';
+import { environment as baseEnvironment } from './environment.base';
+export const environment =  mergeConfigurations(baseEnvironment, {
+  production: true,
+  apiKey: '${GOOGLE_API_KEY}',
+});`;
+
+const configFilePath = path.join(__dirname, '../src/environments/environment.prod.ts');
+fs.writeFileSync(configFilePath, configContent);
