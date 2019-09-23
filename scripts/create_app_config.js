2b8998542608eb0ed1950627a2b7bdc544c01fcd
Aaron Neugebauer <aaron.n@rushdigital.co.nz>
2019-09-23 16:01:47 +1200

    Added theme parameter to builds.

--- scripts/create_app_config.js
+++ scripts/create_app_config.js
@@ -5,9 +5,15 @@ const GOOGLE_API_KEY = process.argv[2];
 if(!GOOGLE_API_KEY) {
     throw new Error('Google API key not set');
 }
+const THEME = process.argv[3] || 'pink';
 
 const configFilePath = path.join(__dirname, '../app/src/environments/environment.prod.ts');
-let configContent = fs.readFileSync(configFilePath);
-configContent.replace('<GOOGLE_API_KEY>', GOOGLE_API_KEY);
-
+let configContent = fs.readFileSync(configFilePath, { 'encoding': 'utf-8' });
+configContent = configContent.replace('<GOOGLE_API_KEY>', GOOGLE_API_KEY);
 fs.writeFileSync(configFilePath, configContent);
+
+const themeFilePath = path.join(__dirname, '../app/src/style/theme.scss');
+let themeContent = `@import 'theme-${THEME}';`;
+fs.writeFileSync(themeFilePath, themeContent);
+
+
