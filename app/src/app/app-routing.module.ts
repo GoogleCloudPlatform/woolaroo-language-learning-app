6876872e8acf1cf7966fbea86ee526535c491eb5
Aaron Neugebauer <aaron.n@rushdigital.co.nz>
2019-09-25 15:06:47 +1200

    Added sidenav.

--- app/src/app/app-routing.module.ts
+++ app/src/app/app-routing.module.ts
@@ -1,20 +1,22 @@
 import {NgModule} from '@angular/core';
 import {Routes, RouterModule} from '@angular/router';
 import {SplashPageComponent} from 'pages/splash/splash';
-import {CapturePage} from 'pages/capture/capture';
+import { CapturePageComponent } from 'pages/capture/capture';
 import {TranslatePage} from 'pages/translate/translate';
 import { FeedbackPageComponent } from 'pages/feedback/feedback';
 
 
 const routes: Routes = [
   { path: '', component: SplashPageComponent },
-  { path: 'capture', component: CapturePage },
+  { path: 'capture', component: CapturePageComponent },
   { path: 'translate', component: TranslatePage },
   { path: 'feedback', component: FeedbackPageComponent }
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [
+    RouterModule.forRoot(routes)
+  ],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- app/src/pages/capture/capture.ts
+++ app/src/pages/capture/capture.ts
@@ -1,47 +1,57 @@
-import {Component, Inject, ViewChild} from '@angular/core';
+import {AfterViewInit, Component, Inject, ViewChild} from '@angular/core';
 import { MatDialog } from '@angular/material';
-import { Router } from "@angular/router";
-import { CameraPreviewComponent, CameraPreviewStatus } from "components/camera-preview/camera-preview";
+import { Router } from '@angular/router';
+import { CameraPreviewComponent, CameraPreviewStatus } from 'components/camera-preview/camera-preview';
 import { ErrorPopUpComponent } from 'components/error-popup/error-popup';
-import { ANALYTICS_SERVICE, IAnalyticsService } from "services/analytics";
+import { ANALYTICS_SERVICE, IAnalyticsService } from 'services/analytics';
 
 @Component({
-  selector: 'page-capture',
+  selector: 'app-page-capture',
   templateUrl: 'capture.html',
   styleUrls: ['./capture.scss']
 })
-export class CapturePage {
+export class CapturePageComponent implements AfterViewInit {
   @ViewChild(CameraPreviewComponent, {static: false})
-  private cameraPreview:CameraPreviewComponent|null = null;
-  public captureInProgress:boolean = false;
+  private cameraPreview: CameraPreviewComponent|null = null;
+  public captureInProgress = false;
+  public sidenavOpen = false;
 
-  constructor( private router:Router,
-               private dialog:MatDialog,
-               @Inject(ANALYTICS_SERVICE) private analyticsService:IAnalyticsService ) {
+  constructor( private router: Router,
+               private dialog: MatDialog,
+               @Inject(ANALYTICS_SERVICE) private analyticsService: IAnalyticsService ) {
   }
 
   ngAfterViewInit() {
     this.analyticsService.logPageView(this.router.url, 'Capture');
-    this.cameraPreview!.start().then(
-      () => console.log("Camera started"),
+    if (!this.cameraPreview) {
+      console.error('Camera preview not found');
+      this.router.navigateByUrl('/', { replaceUrl: true });
+      return;
+    }
+    this.cameraPreview.start().then(
+      () => console.log('Camera started'),
       err => {
-        console.warn("Error starting camera", err);
+        console.warn('Error starting camera', err);
+        return;
         const errorDialog = this.dialog.open(ErrorPopUpComponent, { data: { message: 'Unable to start camera' } });
         errorDialog.afterClosed().subscribe(() => {
-          this.router.navigate(["/"], { replaceUrl: true });
+          this.router.navigateByUrl('/', { replaceUrl: true });
         });
       }
     );
   }
 
   onCaptureClick() {
-    if(this.cameraPreview!.status !== CameraPreviewStatus.Started) {
+    if (!this.cameraPreview) {
+      return;
+    }
+    if (this.cameraPreview.status !== CameraPreviewStatus.Started) {
       return;
     }
     this.captureInProgress = true;
-    this.cameraPreview!.capture().then(
+    this.cameraPreview.capture().then(
       image => {
-        console.log("Image captured");
+        console.log('Image captured');
         this.router.navigate(['/translate'], { state: {capturedImage: image} });
       },
       err => {
@@ -49,10 +59,18 @@ export class CapturePage {
         this.captureInProgress = false;
         this.onCaptureError(err);
       }
-    )
+    );
   }
 
-  onCaptureError(err:any) {
+  onCaptureError(err: any) {
     // TODO
+  }
+
+  onOpenMenuClick() {
+    this.sidenavOpen = !this.sidenavOpen;
+  }
+
+  onSidenavClosed() {
+    this.sidenavOpen = false;
   }
 }
