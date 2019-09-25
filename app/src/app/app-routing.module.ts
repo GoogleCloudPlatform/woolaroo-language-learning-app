6c243ca558a4f0a094352240692da46569405a8e
Aaron Neugebauer <aaron.n@rushdigital.co.nz>
2019-09-25 12:56:36 +1200

    Added feedback types.

--- app/src/app/app-routing.module.ts
+++ app/src/app/app-routing.module.ts
@@ -3,14 +3,14 @@ import {Routes, RouterModule} from '@angular/router';
 import {SplashPageComponent} from 'pages/splash/splash';
 import {CapturePage} from 'pages/capture/capture';
 import {TranslatePage} from 'pages/translate/translate';
-import {FeedbackPage} from 'pages/feedback/feedback';
+import { FeedbackPageComponent } from 'pages/feedback/feedback';
 
 
 const routes: Routes = [
   { path: '', component: SplashPageComponent },
   { path: 'capture', component: CapturePage },
   { path: 'translate', component: TranslatePage },
-  { path: 'feedback', component: FeedbackPage }
+  { path: 'feedback', component: FeedbackPageComponent }
 ];
 
 @NgModule({
