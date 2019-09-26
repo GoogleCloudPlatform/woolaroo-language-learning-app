f90d55ca5e2023d9b42ea6ec9b611f9ec7133918
Aaron Neugebauer <aaron.n@rushdigital.co.nz>
2019-09-27 11:38:38 +1200

    Renamed landing page.

---
+++ app/src/pages/splash/splash.ts
@@ -0,0 +1,22 @@
+import {AfterViewInit, Component, Inject} from '@angular/core';
+import {Router} from '@angular/router';
+import {IAnalyticsService, ANALYTICS_SERVICE} from 'services/analytics';
+
+@Component({
+  selector: 'app-page-splash',
+  templateUrl: 'splash.html',
+  styleUrls: ['./splash.scss']
+})
+export class SplashPageComponent implements AfterViewInit {
+  constructor( private router: Router,
+               @Inject(ANALYTICS_SERVICE) private analyticsService: IAnalyticsService ) {
+  }
+
+  ngAfterViewInit() {
+    this.analyticsService.logPageView(this.router.url, 'Capture');
+  }
+
+  onStartClick() {
+    this.router.navigate(['/capture']);
+  }
+}
