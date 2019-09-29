e792b299b972f60a88d48135be5972d675613dc3
Aaron Neugebauer <aaron.n@rushdigital.co.nz>
2019-09-30 10:24:47 +1300

    Updated capture screen designs.

--- app/src/components/icon/icon.ts
+++ app/src/components/icon/icon.ts
@@ -2,7 +2,7 @@ import {Component, Input} from '@angular/core';
 import {MatIconRegistry} from '@angular/material';
 import {DomSanitizer} from '@angular/platform-browser';
 
-export type Icon = 'capture_photo'|'menu'|'close';
+export type Icon = 'photo_library'|'capture_photo'|'menu'|'close';
 
 @Component({
   selector: 'app-icon',
@@ -18,8 +18,9 @@ export class IconComponent {
   }
 
   public static registerIcons(iconRegistry: MatIconRegistry, domSanitizer: DomSanitizer) {
-    iconRegistry.addSvgIcon('capture_photo', domSanitizer.bypassSecurityTrustResourceUrl('/assets/icons/camera_alt.svg'));
+    iconRegistry.addSvgIcon('capture_photo', domSanitizer.bypassSecurityTrustResourceUrl('/assets/icons/radio_button_unchecked.svg'));
     iconRegistry.addSvgIcon('menu', domSanitizer.bypassSecurityTrustResourceUrl('/assets/icons/menu.svg'));
     iconRegistry.addSvgIcon('close', domSanitizer.bypassSecurityTrustResourceUrl('/assets/icons/close.svg'));
+    iconRegistry.addSvgIcon('photo_library', domSanitizer.bypassSecurityTrustResourceUrl('/assets/icons/photo_library.svg'));
   }
 }
