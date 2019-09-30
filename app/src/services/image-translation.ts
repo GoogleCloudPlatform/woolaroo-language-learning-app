ccf916e223a8bdf73696edda5f3f84e7af29878e
Aaron Neugebauer <aaron.n@rushdigital.co.nz>
2019-09-30 16:50:59 +1300

    Updated translation endpoint to batch requests.

--- app/src/services/image-translation.ts
+++ app/src/services/image-translation.ts
@@ -1,15 +1,15 @@
-import {Inject, Injectable} from "@angular/core";
-import { WordTranslation } from "./entities/translation";
-import { IImageRecognitionService, IMAGE_RECOGNITION_SERVICE } from "./image-recognition";
-import { ITranslationService, TRANSLATION_SERVICE } from "./translation";
+import {Inject, Injectable} from '@angular/core';
+import { WordTranslation } from './entities/translation';
+import { IImageRecognitionService, IMAGE_RECOGNITION_SERVICE } from './image-recognition';
+import { ITranslationService, TRANSLATION_SERVICE } from './translation';
 
 @Injectable()
 export class ImageTranslationService {
-  constructor(@Inject(IMAGE_RECOGNITION_SERVICE) private imageRecognitionService:IImageRecognitionService,
-              @Inject(TRANSLATION_SERVICE) private translationService:ITranslationService) {
+  constructor(@Inject(IMAGE_RECOGNITION_SERVICE) private imageRecognitionService: IImageRecognitionService,
+              @Inject(TRANSLATION_SERVICE) private translationService: ITranslationService) {
   }
 
-  public async loadTranslatedDescriptions(imageData:Blob, maxTranslations:number = 0):Promise<WordTranslation[]> {
+  public async loadTranslatedDescriptions(imageData: Blob, maxTranslations: number = 0): Promise<WordTranslation[]> {
     const descriptions = await this.imageRecognitionService.loadDescriptions(imageData);
     return await this.translationService.translate(descriptions.map(d => d.description), maxTranslations);
   }
