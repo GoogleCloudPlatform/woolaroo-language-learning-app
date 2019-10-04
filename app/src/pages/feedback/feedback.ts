fc198121604d06781a3d93fddb53de8af1fe225a
Aaron Neugebauer <aaron.n@rushdigital.co.nz>
2019-10-04 14:54:45 +1300

    Fixed feedback form values.

--- app/src/pages/feedback/feedback.ts
+++ app/src/pages/feedback/feedback.ts
@@ -17,7 +17,6 @@ export class FeedbackPageComponent implements AfterViewInit {
   private readonly feedbackForm: FormGroup;
   public submittingForm = false;
   public FeedbackType = FeedbackType;
-  public feedback: Feedback = { content: '', types: [] };
 
   constructor( private router: Router,
                private location: Location,
@@ -48,7 +47,8 @@ export class FeedbackPageComponent implements AfterViewInit {
     }
     this.submittingForm = true;
     const loadingPopup = this.dialog.open(LoadingPopUpComponent);
-    this.feedbackService.sendFeedback(this.feedback).then(
+    console.log(this.feedbackForm.value);
+    this.feedbackService.sendFeedback(this.feedbackForm.value).then(
       () => {
         console.log('Feedback submitted');
         this.location.back();
@@ -64,19 +64,24 @@ export class FeedbackPageComponent implements AfterViewInit {
     );
   }
 
+  typeIsSelected(type: FeedbackType): boolean {
+    return this.feedbackForm.controls.types.value.indexOf(type) >= 0;
+  }
+
   onFeedbackTypeChange(ev: MatCheckboxChange, type: FeedbackType) {
-    const index = this.feedback.types.indexOf(type);
+    const ctl = this.feedbackForm.controls.types;
+    const types = ctl.value;
+    const index = types.indexOf(type);
     if (ev.checked) {
       if (index < 0) {
-        this.feedback.types.push(type);
+        types.push(type);
       }
     } else if (index >= 0) {
-      this.feedback.types.splice(index, 1);
+      types.splice(index, 1);
     }
-    const ctl = this.feedbackForm.controls.types;
     ctl.markAsDirty();
     ctl.markAsTouched();
-    ctl.setValue(this.feedback.types);
+    ctl.setValue(types);
   }
 
   onCloseClick() {
