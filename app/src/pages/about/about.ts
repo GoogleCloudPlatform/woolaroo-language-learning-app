2fc9755a8197a7e6cc773735b76682df999dd03a
Aaron Neugebauer <aaron.n@rushdigital.co.nz>
2019-10-04 15:57:04 +1300

    Added about page.

---
+++ app/src/pages/about/about.ts
@@ -0,0 +1,22 @@
+import { AfterViewInit, Component, Inject } from '@angular/core';
+import { Router } from '@angular/router';
+import { IAnalyticsService, ANALYTICS_SERVICE } from 'services/analytics';
+
+@Component({
+  selector: 'app-page-about',
+  templateUrl: './about.html',
+  styleUrls: ['./about.scss']
+})
+export class AboutPageComponent implements AfterViewInit {
+  constructor( private router: Router,
+               @Inject(ANALYTICS_SERVICE) private analyticsService: IAnalyticsService ) {
+  }
+
+  ngAfterViewInit() {
+    this.analyticsService.logPageView(this.router.url, 'About');
+  }
+
+  onCloseClick() {
+    history.back();
+  }
+}
