4d3557e815f78045381674cdeb205732b5cc1892
Aaron Neugebauer <aaron.n@rushdigital.co.nz>
2019-10-04 14:50:06 +1300

    Updated styles on feedback form.

--- app/src/pages/feedback/feedback.module.ts
+++ app/src/pages/feedback/feedback.module.ts
@@ -1,23 +1,32 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatToolbarModule } from '@angular/material';
+import { ErrorStateMatcher, ShowOnDirtyErrorStateMatcher } from '@angular/material';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCheckboxModule } from '@angular/material/checkbox';
 import { MatInputModule } from '@angular/material/input';
 import { LoadingPopUpModule } from 'components/loading-popup/loading-popup.module';
 import { FeedbackPageComponent } from './feedback';
+import { IconComponentModule } from 'components/icon/icon.module';
 
 @NgModule({
   declarations: [
     FeedbackPageComponent,
   ],
+  providers: [
+    { provide: ErrorStateMatcher, useClass: ShowOnDirtyErrorStateMatcher }
+  ],
   imports: [
     CommonModule,
+    MatToolbarModule,
     MatButtonModule,
     MatCheckboxModule,
     MatInputModule,
     FormsModule,
-    LoadingPopUpModule
+    ReactiveFormsModule,
+    LoadingPopUpModule,
+    IconComponentModule
   ]
 })
 export class FeedbackPageModule {}
--- app/src/pages/feedback/feedback.ts
+++ app/src/pages/feedback/feedback.ts
@@ -1,12 +1,12 @@
-import {AfterViewInit, Component, Inject} from '@angular/core';
-import {Location} from '@angular/common';
-import {Router} from '@angular/router';
-import {NgForm} from '@angular/forms';
-import {MatCheckboxChange, MatDialog} from '@angular/material';
-import {ANALYTICS_SERVICE, IAnalyticsService} from 'services/analytics';
-import {FEEDBACK_SERVICE, IFeedbackService} from 'services/feedback';
-import {FeedbackType} from 'services/entities/feedback';
-import {LoadingPopUpComponent} from 'components/loading-popup/loading-popup';
+import { AfterViewInit, Component, Inject } from '@angular/core';
+import { Location } from '@angular/common';
+import { Router } from '@angular/router';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { MatCheckboxChange, MatDialog } from '@angular/material';
+import { ANALYTICS_SERVICE, IAnalyticsService } from 'services/analytics';
+import { FEEDBACK_SERVICE, IFeedbackService } from 'services/feedback';
+import { FeedbackType, Feedback } from 'services/entities/feedback';
+import { LoadingPopUpComponent } from 'components/loading-popup/loading-popup';
 
 @Component({
   selector: 'app-page-feedback',
@@ -14,28 +14,41 @@ import {LoadingPopUpComponent} from 'components/loading-popup/loading-popup';
   styleUrls: ['./feedback.scss']
 })
 export class FeedbackPageComponent implements AfterViewInit {
+  private readonly feedbackForm: FormGroup;
   public submittingForm = false;
   public FeedbackType = FeedbackType;
-  public feedbackTypes: FeedbackType[] = [];
+  public feedback: Feedback = { content: '', types: [] };
 
   constructor( private router: Router,
                private location: Location,
                private dialog: MatDialog,
                @Inject(FEEDBACK_SERVICE) private feedbackService: IFeedbackService,
                @Inject(ANALYTICS_SERVICE) private analyticsService: IAnalyticsService ) {
+    this.feedbackForm = new FormGroup({
+      content: new FormControl('', [
+        Validators.required
+      ]),
+      types: new FormControl([], [
+        (ctl) => ctl.dirty && (!ctl.value || ctl.value.length === 0) ? { required: true } : null
+      ])
+    });
   }
 
   ngAfterViewInit() {
     this.analyticsService.logPageView(this.router.url, 'Feedback');
   }
 
-  onFormSubmit(ev: Event, form: NgForm) {
-    if (!form.valid) {
+  onFormSubmit() {
+    // Force types checkboxes to validate
+    const typesControl = this.feedbackForm.controls.types;
+    typesControl.markAsDirty();
+    typesControl.updateValueAndValidity();
+    if (!this.feedbackForm.valid) {
       return;
     }
     this.submittingForm = true;
     const loadingPopup = this.dialog.open(LoadingPopUpComponent);
-    this.feedbackService.sendFeedback({ content: form.value.content, types: this.feedbackTypes }).then(
+    this.feedbackService.sendFeedback(this.feedback).then(
       () => {
         console.log('Feedback submitted');
         this.location.back();
@@ -52,13 +65,21 @@ export class FeedbackPageComponent implements AfterViewInit {
   }
 
   onFeedbackTypeChange(ev: MatCheckboxChange, type: FeedbackType) {
-    const index = this.feedbackTypes.indexOf(type);
+    const index = this.feedback.types.indexOf(type);
     if (ev.checked) {
       if (index < 0) {
-        this.feedbackTypes.push(type);
+        this.feedback.types.push(type);
       }
     } else if (index >= 0) {
-      this.feedbackTypes.splice(index, 1);
+      this.feedback.types.splice(index, 1);
     }
+    const ctl = this.feedbackForm.controls.types;
+    ctl.markAsDirty();
+    ctl.markAsTouched();
+    ctl.setValue(this.feedback.types);
+  }
+
+  onCloseClick() {
+    history.back();
   }
 }
