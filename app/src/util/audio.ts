e158799ce6ec23bcf908edbfb667902dd8d5cd1c
Aaron Neugebauer <aaron.n@rushdigital.co.nz>
2019-10-09 11:40:27 +1300

    Changed audio recording to use MediaRecorder API.

--- app/src/util/audio.ts
+++ app/src/util/audio.ts
@@ -1,65 +1,71 @@
-const audioContext = AudioContext || (window as any).webkitAudioContext;
+/// <reference types="@types/dom-mediacapture-record" />
 
 export interface AudioStream {
   stop(): void;
 }
 
 export interface PlaybackStream extends AudioStream {
-  setEndedListener(onEnded: () => void): void;
+  onended: (() => void)|null;
+  getDuration(): number;
+  getCurrentTime(): number;
 }
 
-type RecordingEndedListener = (buffer: Float32Array, duration: number) => void;
+type RecordingEndedListener = (buffer: Blob) => void;
 
 export interface RecordingStream extends AudioStream {
-  setEndedListener(onEnded: RecordingEndedListener): void;
+  onended: RecordingEndedListener|null;
 }
 
-export function startRecording(bufferSize: number): Promise<RecordingStream> {
+function getSupportedMimeType(mimeTypes: string[]): string|null {
+  for (const type of mimeTypes) {
+    if (MediaRecorder.isTypeSupported(type)) {
+      return type;
+    }
+  }
+  return null;
+}
+
+export function startRecording(bufferSize: number, mimeTypes?: string[]): Promise<RecordingStream> {
   if (!navigator.mediaDevices || !navigator.mediaDevices.getUserMedia) {
     return Promise.reject(new Error('Browser does not support audio recording'));
   }
   return new Promise((resolve, reject) => {
     navigator.mediaDevices.getUserMedia({video: false, audio: true}).then(
       (stream) => {
-        let buffer: Float32Array = new Float32Array();
-        const context = new audioContext();
-        const streamNode = context.createMediaStreamSource(stream);
-        const processorNode = context.createScriptProcessor(bufferSize, 1, 1);
-        const audioTrack = stream.getAudioTracks()[0];
-        let endedListener: RecordingEndedListener|null = null;
-        audioTrack.addEventListener('ended', () => {
-          // audio recording ended unexpectedly
-          streamNode.disconnect();
-          processorNode.disconnect();
-          if (endedListener) {
-            endedListener(buffer, buffer.length / context.sampleRate);
+        const options: MediaRecorderOptions = {};
+        if (mimeTypes) {
+          const type = getSupportedMimeType(mimeTypes);
+          if (type) {
+            options.mimeType = type;
+          } else {
+            console.warn('No supported mime type found - using default type');
           }
-          context.close();
-        });
-        processorNode.addEventListener('audioprocess', (ev) => {
-          const channelData = ev.inputBuffer.getChannelData(0);
-          const newBuffer = new Float32Array(buffer.length + channelData.length);
-          newBuffer.set(buffer, 0);
-          newBuffer.set(channelData, buffer.length);
-          buffer = newBuffer;
-        });
-        streamNode.connect(processorNode);
-        processorNode.connect(context.destination);
-        resolve({
+        }
+        const recorder = new MediaRecorder(stream, options);
+        const recordingStream: RecordingStream = {
+          onended: null,
           stop: () => {
-            // stop audio recording - does not trigger the "ended" event
-            audioTrack.stop();
-            streamNode.disconnect();
-            processorNode.disconnect();
-            if (endedListener) {
-              endedListener(buffer, 1000 * buffer.length / context.sampleRate);
-            }
-            context.close();
-          },
-          setEndedListener: (listener: RecordingEndedListener) => {
-            endedListener = listener;
+            recorder.stop();
           }
-        });
+        };
+        recorder.onstart = () => {
+          console.log('Recording started');
+          resolve(recordingStream);
+        };
+        recorder.onerror = (ev) => {
+          console.warn('Recording error', ev.error);
+          reject(ev.error);
+        };
+        recorder.ondataavailable = (dataEvent: BlobEvent) => {
+          console.log('Recording data available');
+          if (recordingStream.onended) {
+            recordingStream.onended(dataEvent.data);
+          }
+        };
+        recorder.onstop = () => {
+          console.log('Recording stopped');
+        };
+        recorder.start();
       },
       (err) => {
         console.warn('Error creating audio stream', err);
@@ -69,34 +75,33 @@ export function startRecording(bufferSize: number): Promise<RecordingStream> {
   });
 }
 
-export function playBuffer(buffer: Float32Array, duration: number): Promise<PlaybackStream> {
-  return new Promise<any>((resolve) => {
-    const context = new audioContext();
-    let endedListener: (() => void)|null = null;
-    const source = context.createBufferSource();
-    source.addEventListener('ended', () => {
-      source.disconnect();
-      context.close();
-      if (endedListener) {
-        endedListener();
-      }
-    });
-    source.buffer = context.createBuffer(1, buffer.length, Math.round(buffer.length / (duration * 0.001)));
-    source.buffer.copyToChannel(buffer, 0);
-    source.connect(context.destination);
-    source.start();
-    resolve({
-      stop: () => {
-        source.stop();
-        source.disconnect();
-        context.close();
-        if (endedListener) {
-          endedListener();
-        }
+export function play(buffer: Blob): Promise<PlaybackStream> {
+  return new Promise<any>((resolve, reject) => {
+    const audio = new Audio();
+    const stream: PlaybackStream = {
+      onended: null,
+      getCurrentTime: () => {
+        return audio.currentTime;
       },
-      setEndedListener: (listener: (() => void)) => {
-        endedListener = listener;
+      getDuration: () => {
+        return audio.duration;
+      },
+      stop: () => {
+        audio.pause();
+        if (stream.onended) {
+          stream.onended();
+        }
+    }};
+    audio.src = URL.createObjectURL(buffer);
+    audio.addEventListener('ended', () => {
+      URL.revokeObjectURL(audio.src);
+      if (stream.onended) {
+        stream.onended();
       }
     });
+    audio.play().then(
+      () => resolve(stream),
+      reject
+    );
   });
 }
