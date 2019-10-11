bddba5c4d9a2f1a4129863127a3b76c58ffb6646
Aaron Neugebauer <aaron.n@rushdigital.co.nz>
2019-10-11 16:37:39 +1300

    Fixed back navigation state.

--- app/src/pages/translate/translate.module.ts
+++ app/src/pages/translate/translate.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material';
-import { TranslatePageComponent } from './translate';
+import { TranslatePageComponent, TRANSLATE_PAGE_CONFIG } from './translate';
 import { ServicesModule } from 'services/services.module';
 import { IconComponentModule } from 'components/icon/icon.module';
 import { TranslationSelectorModule } from 'components/translation-selector/translation-selector.module';
 import { PipesModule } from 'pipes/pipes.module';
+import { environment } from 'environments/environment';
 
 @NgModule({
   declarations: [
@@ -16,6 +17,9 @@ import { PipesModule } from 'pipes/pipes.module';
     PipesModule,
     TranslationSelectorModule,
     ServicesModule
-  ]
+  ],
+  providers: [
+    { provide: TRANSLATE_PAGE_CONFIG, useValue: environment.pages.translate }
+  ],
 })
 export class TranslatePageModule {}
