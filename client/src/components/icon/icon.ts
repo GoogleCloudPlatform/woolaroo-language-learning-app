9ead23d810f7ae4fc8cd159ef3f943ecf0246e32
Aaron Neugebauer <aaron.n@rushdigital.co.nz>
2019-10-14 16:53:11 +1300

    Fixed asset URLs.

--- client/src/components/icon/icon.ts
+++ client/src/components/icon/icon.ts
@@ -1,6 +1,6 @@
-import {Component, Input} from '@angular/core';
-import {MatIconRegistry} from '@angular/material';
-import {DomSanitizer} from '@angular/platform-browser';
+import { Component, Input } from '@angular/core';
+import { MatIconRegistry } from '@angular/material';
+import { DomSanitizer } from '@angular/platform-browser';
 
 export type Icon = 'play_audio'|'record_audio'|'stop_audio'|'play_recording'|'back'|'add_feedback'|
   'add_to_home'|'brand'|'photo_library'|'capture_photo'|'menu'|'close'|'share';
@@ -21,18 +21,18 @@ export class IconComponent {
   }
 
   public static registerIcons(iconRegistry: MatIconRegistry, domSanitizer: DomSanitizer) {
-    iconRegistry.addSvgIcon('capture_photo', domSanitizer.bypassSecurityTrustResourceUrl('/assets/icons/radio_button_unchecked.svg'));
-    iconRegistry.addSvgIcon('menu', domSanitizer.bypassSecurityTrustResourceUrl('/assets/icons/menu.svg'));
-    iconRegistry.addSvgIcon('close', domSanitizer.bypassSecurityTrustResourceUrl('/assets/icons/close.svg'));
-    iconRegistry.addSvgIcon('photo_library', domSanitizer.bypassSecurityTrustResourceUrl('/assets/icons/photo_library.svg'));
-    iconRegistry.addSvgIcon('brand', domSanitizer.bypassSecurityTrustResourceUrl('/assets/icons/radio_button_unchecked.svg'));
-    iconRegistry.addSvgIcon('add_to_home', domSanitizer.bypassSecurityTrustResourceUrl('/assets/icons/save_alt.svg'));
-    iconRegistry.addSvgIcon('add_feedback', domSanitizer.bypassSecurityTrustResourceUrl('/assets/icons/chat_bubble_outline.svg'));
-    iconRegistry.addSvgIcon('back', domSanitizer.bypassSecurityTrustResourceUrl('/assets/icons/arrow_back.svg'));
-    iconRegistry.addSvgIcon('share', domSanitizer.bypassSecurityTrustResourceUrl('/assets/icons/share.svg'));
-    iconRegistry.addSvgIcon('play_audio', domSanitizer.bypassSecurityTrustResourceUrl('/assets/icons/volume_up.svg'));
-    iconRegistry.addSvgIcon('record_audio', domSanitizer.bypassSecurityTrustResourceUrl('/assets/icons/mic.svg'));
-    iconRegistry.addSvgIcon('stop_audio', domSanitizer.bypassSecurityTrustResourceUrl('/assets/icons/stop.svg'));
-    iconRegistry.addSvgIcon('play_recording', domSanitizer.bypassSecurityTrustResourceUrl('/assets/icons/play_arrow.svg'));
+    iconRegistry.addSvgIcon('capture_photo', domSanitizer.bypassSecurityTrustResourceUrl('assets/icons/radio_button_unchecked.svg'));
+    iconRegistry.addSvgIcon('menu', domSanitizer.bypassSecurityTrustResourceUrl('assets/icons/menu.svg'));
+    iconRegistry.addSvgIcon('close', domSanitizer.bypassSecurityTrustResourceUrl('assets/icons/close.svg'));
+    iconRegistry.addSvgIcon('photo_library', domSanitizer.bypassSecurityTrustResourceUrl('assets/icons/photo_library.svg'));
+    iconRegistry.addSvgIcon('brand', domSanitizer.bypassSecurityTrustResourceUrl('assets/icons/radio_button_unchecked.svg'));
+    iconRegistry.addSvgIcon('add_to_home', domSanitizer.bypassSecurityTrustResourceUrl('assets/icons/save_alt.svg'));
+    iconRegistry.addSvgIcon('add_feedback', domSanitizer.bypassSecurityTrustResourceUrl('assets/icons/chat_bubble_outline.svg'));
+    iconRegistry.addSvgIcon('back', domSanitizer.bypassSecurityTrustResourceUrl('assets/icons/arrow_back.svg'));
+    iconRegistry.addSvgIcon('share', domSanitizer.bypassSecurityTrustResourceUrl('assets/icons/share.svg'));
+    iconRegistry.addSvgIcon('play_audio', domSanitizer.bypassSecurityTrustResourceUrl('assets/icons/volume_up.svg'));
+    iconRegistry.addSvgIcon('record_audio', domSanitizer.bypassSecurityTrustResourceUrl('assets/icons/mic.svg'));
+    iconRegistry.addSvgIcon('stop_audio', domSanitizer.bypassSecurityTrustResourceUrl('assets/icons/stop.svg'));
+    iconRegistry.addSvgIcon('play_recording', domSanitizer.bypassSecurityTrustResourceUrl('assets/icons/play_arrow.svg'));
   }
 }
