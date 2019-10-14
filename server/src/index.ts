5911984e195abea849fe8ac55f81c9580e12e222
Aaron Neugebauer <aaron.n@rushdigital.co.nz>
2019-10-14 17:54:37 +1300

    Removed HTTPS redirect.

--- server/src/index.ts
+++ server/src/index.ts
@@ -4,14 +4,6 @@ import { default as path } from 'path';
 
 const app = express();
 
-app.use((req, res, next) => {
-  if (req.secure || req.headers['X-Forwarded-Proto'] === 'https') {
-    next();
-  } else {
-    res.redirect('https://' + req.headers.host + req.url);
-  }
-});
-
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
