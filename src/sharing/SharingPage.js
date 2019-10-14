ecfd45586738f0657f7f0a9865495e4f95dbc888
Marco Rosas <marcorosas@google.com>
2019-10-14 15:42:36 -0700

    Beautify file

--- src/sharing/SharingPage.js
+++ src/sharing/SharingPage.js
@@ -1,7 +1,7 @@
 import {makeStyles} from "@material-ui/core/styles";
-import Button from '@material-ui/core/Button';
-import React from 'react';
-import TextField from '@material-ui/core/TextField';
+import Button from "@material-ui/core/Button";
+import React from "react";
+import TextField from "@material-ui/core/TextField";
 
 const useStyles = makeStyles(theme => ({
   textField: {
@@ -10,53 +10,83 @@ const useStyles = makeStyles(theme => ({
     width: 350
   },
   button: {
-  margin: theme.spacing(1),
-},
+    margin: theme.spacing(1)
+  }
 }));
 
 function SharingPage() {
   const classes = useStyles();
-  return (<div>
-    <h2>Share with friends</h2>
-
+  return (
     <div>
-      <TextField id="share-emails" label="Invite people to contribute" style={{
-          margin: 8
-        }} placeholder="Enter comma separated emails" className={classes.textField} InputLabelProps={{
-          shrink: true
-        }}/>
-    </div>
+      <h2>Share with friends</h2>
 
-    <div>
-      <TextField id="optional-message" multiline="multiline" rows="4" placeholder="Write message (optional)" className={classes.textField} margin="normal"/>
-    </div>
+      <div>
+        <TextField
+          id="share-emails"
+          label="Invite people to contribute"
+          style={{
+            margin: 8
+          }}
+          placeholder="Enter comma separated emails"
+          className={classes.textField}
+          InputLabelProps={{
+            shrink: true
+          }}
+        />
+      </div>
 
-    <Button variant="contained" color="primary" className={classes.button}>
+      <div>
+        <TextField
+          id="optional-message"
+          multiline="multiline"
+          rows="4"
+          placeholder="Write message (optional)"
+          className={classes.textField}
+          margin="normal"
+        />
+      </div>
+
+      <Button variant="contained" color="primary" className={classes.button}>
         Send Invite
       </Button>
 
-    <div>
-      <TextField id="app-link" label="App URL" style={{
-          margin: 8
-        }} placeholder="ladino.appspot.com" className={classes.textField} InputLabelProps={{
-          shrink: true
-        }}/>
-    </div>
-    <Button variant="contained" color="primary" className={classes.button}>
+      <div>
+        <TextField
+          id="app-link"
+          label="App URL"
+          style={{
+            margin: 8
+          }}
+          placeholder="ladino.appspot.com"
+          className={classes.textField}
+          InputLabelProps={{
+            shrink: true
+          }}
+        />
+      </div>
+      <Button variant="contained" color="primary" className={classes.button}>
         Copy link
       </Button>
 
       <div>
-        <TextField id="contributor-app-link" label="Contributor app URL" style={{
+        <TextField
+          id="contributor-app-link"
+          label="Contributor app URL"
+          style={{
             margin: 8
-          }} placeholder="ladino.appspot.com/contribute" className={classes.textField} InputLabelProps={{
+          }}
+          placeholder="ladino.appspot.com/contribute"
+          className={classes.textField}
+          InputLabelProps={{
             shrink: true
-          }}/>
+          }}
+        />
       </div>
       <Button variant="contained" color="primary" className={classes.button}>
-          Copy link
-        </Button>
-  </div>);
+        Copy link
+      </Button>
+    </div>
+  );
 }
 
 export default SharingPage;
