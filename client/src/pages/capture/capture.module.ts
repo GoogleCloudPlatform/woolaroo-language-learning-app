dc3c21a99780db9ebda951ed86190bcbef0dec4b
Aaron Neugebauer <aaron.n@rushdigital.co.nz>
2019-10-21 14:51:15 +1300

    Updated opening and closing of loading modals.

--- client/src/pages/capture/capture.module.ts
+++ client/src/pages/capture/capture.module.ts
@@ -4,6 +4,7 @@ import { CapturePageComponent } from './capture';
 import { CameraPreviewModule } from 'components/camera-preview/camera-preview.module';
 import { IconComponentModule } from 'components/icon/icon.module';
 import { ErrorPopUpModule } from 'components/error-popup/error-popup.module';
+import { LoadingPopUpModule } from 'components/loading-popup/loading-popup.module';
 import { SidenavModule } from 'components/sidenav/sidenav.module';
 import { FileUploadButtonModule } from 'components/file-upload-button/file-upload-button.module';
 import { CapturePopUpModule } from 'components/capture-popup/capture-popup.module';
@@ -17,6 +18,7 @@ import { CapturePopUpModule } from 'components/capture-popup/capture-popup.modul
     IconComponentModule,
     CameraPreviewModule,
     ErrorPopUpModule,
+    LoadingPopUpModule,
     CapturePopUpModule,
     SidenavModule,
     FileUploadButtonModule
--- client/src/pages/photo-source/photo-source.ts
+++ client/src/pages/photo-source/photo-source.ts
@@ -5,6 +5,7 @@ import { IAnalyticsService, ANALYTICS_SERVICE } from 'services/analytics';
 import { IImageRecognitionService, IMAGE_RECOGNITION_SERVICE } from 'services/image-recognition';
 import { LoadingPopUpComponent } from 'components/loading-popup/loading-popup';
 import { AppRoutes } from 'app/routes';
+import { SessionService } from 'services/session';
 
 @Component({
   selector: 'app-page-photo-source',
@@ -14,6 +15,7 @@ import { AppRoutes } from 'app/routes';
 export class PhotoSourcePageComponent implements AfterViewInit {
   constructor( private router: Router,
                private dialog: MatDialog,
+               private sessionService: SessionService,
                @Inject(ANALYTICS_SERVICE) private analyticsService: IAnalyticsService,
                @Inject(IMAGE_RECOGNITION_SERVICE) private imageRecognitionService: IImageRecognitionService ) {
   }
@@ -23,30 +25,49 @@ export class PhotoSourcePageComponent implements AfterViewInit {
   }
 
   onImageUploaded(image: Blob) {
-    const loadingPopUp = this.dialog.open(LoadingPopUpComponent);
-    this.imageRecognitionService.loadDescriptions(image).then(
-      (descriptions) => {
-        if (descriptions.length > 0) {
-          this.router.navigateByUrl(AppRoutes.Translate, { state: { image, words: descriptions.map(d => d.description) } }).finally(
-            () => loadingPopUp.close()
-          );
-        } else {
-          this.router.navigateByUrl(AppRoutes.CaptionImage, { state: { image } }).finally(
-            () => loadingPopUp.close()
-          );
-        }
-      },
-      (err) => {
-        console.warn('Error loading image descriptions', err);
-        loadingPopUp.close();
-        this.router.navigateByUrl(AppRoutes.CaptionImage, { state: { image } }).finally(
-          () => loadingPopUp.close()
+    const loadingPopUp = this.dialog.open(LoadingPopUpComponent, { closeOnNavigation: false, disableClose: true });
+    this.sessionService.currentSession.currentModal = loadingPopUp;
+    loadingPopUp.beforeClosed().subscribe({
+      next: () => this.sessionService.currentSession.currentModal = null
+    });
+    loadingPopUp.afterOpened().subscribe({
+      next: () => {
+        this.imageRecognitionService.loadDescriptions(image).then(
+          (descriptions) => {
+            if (descriptions.length > 0) {
+              this.router.navigateByUrl(AppRoutes.Translate, { state: { image, words: descriptions.map(d => d.description) } }).then(
+                (success) => {
+                  if (!success) {
+                    loadingPopUp.close();
+                  }
+                },
+                () => loadingPopUp.close()
+              );
+            } else {
+              this.router.navigateByUrl(AppRoutes.CaptionImage, { state: { image } }).finally(
+                () => loadingPopUp.close()
+              );
+            }
+          },
+          (err) => {
+            console.warn('Error loading image descriptions', err);
+            this.router.navigateByUrl(AppRoutes.CaptionImage, { state: { image } }).finally(
+              () => loadingPopUp.close()
+            );
+          }
         );
       }
-    );
+    });
   }
 
   onCaptureClick() {
-    this.router.navigateByUrl(AppRoutes.CaptureImage);
+    const loadingPopUp = this.dialog.open(LoadingPopUpComponent, { closeOnNavigation: false, disableClose: true });
+    this.sessionService.currentSession.currentModal = loadingPopUp;
+    loadingPopUp.beforeClosed().subscribe({
+      next: () => this.sessionService.currentSession.currentModal = null
+    });
+    loadingPopUp.afterOpened().subscribe({
+      next: () => this.router.navigateByUrl(AppRoutes.CaptureImage)
+    });
   }
 }
