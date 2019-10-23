2af2d94cd99f07029fbdd1b591fcfdaf05d1a46c
Aaron Neugebauer <aaron.n@rushdigital.co.nz>
2019-10-23 16:52:02 +1300

    Added fallback recording option.

--- client/src/util/audio.ts
+++ client/src/util/audio.ts
@@ -25,47 +25,26 @@ function getSupportedMimeType(mimeTypes: string[]): string|null {
   return null;
 }
 
-export function startRecording(bufferSize: number, mimeTypes?: string[]): Promise<RecordingStream> {
+export async function startRecording(bufferSize: number, mimeTypes?: string[]): Promise<RecordingStream> {
   if (!navigator.mediaDevices || !navigator.mediaDevices.getUserMedia) {
-    return Promise.reject(new Error('Browser does not support audio recording'));
+    throw new Error('Browser does not support audio recording');
   }
-  return new Promise((resolve, reject) => {
+  return new Promise<RecordingStream>((resolve, reject) => {
     navigator.mediaDevices.getUserMedia({video: false, audio: true}).then(
       (stream) => {
-        const options: MediaRecorderOptions = {};
-        if (mimeTypes) {
-          const type = getSupportedMimeType(mimeTypes);
-          if (type) {
-            options.mimeType = type;
-          } else {
-            console.warn('No supported mime type found - using default type');
-          }
+        if (MediaRecorder) {
+          console.log('Starting MediaRecorder recording');
+          startMediaRecorderRecording(stream, bufferSize, mimeTypes).then(
+            str => resolve(str),
+            ex => reject(ex)
+          );
+        } else {
+          console.log('Starting AudioContext recording');
+          startAudioContextRecording(stream, bufferSize).then(
+            str => resolve(str),
+            ex => reject(ex)
+          );
         }
-        const recorder = new MediaRecorder(stream, options);
-        const recordingStream: RecordingStream = {
-          onended: null,
-          stop: () => {
-            recorder.stop();
-          }
-        };
-        recorder.onstart = () => {
-          console.log('Recording started');
-          resolve(recordingStream);
-        };
-        recorder.onerror = (ev) => {
-          console.warn('Recording error', ev.error);
-          reject(ev.error);
-        };
-        recorder.ondataavailable = (dataEvent: BlobEvent) => {
-          console.log('Recording data available');
-          if (recordingStream.onended) {
-            recordingStream.onended(dataEvent.data);
-          }
-        };
-        recorder.onstop = () => {
-          console.log('Recording stopped');
-        };
-        recorder.start();
       },
       (err) => {
         console.warn('Error creating audio stream', err);
@@ -73,6 +52,129 @@ export function startRecording(bufferSize: number, mimeTypes?: string[]): Promis
       }
     );
   });
+}
+
+function startMediaRecorderRecording(stream: MediaStream, bufferSize: number, mimeTypes?: string[]): Promise<RecordingStream> {
+  const options: MediaRecorderOptions = {};
+  if (mimeTypes) {
+    const type = getSupportedMimeType(mimeTypes);
+    if (type) {
+      options.mimeType = type;
+    } else {
+      console.warn('No supported mime type found - using default type');
+    }
+  }
+  const recorder = new MediaRecorder(stream, options);
+  const recordingStream: RecordingStream = {
+    onended: null,
+    stop: () => {
+      recorder.stop();
+    }
+  };
+  recorder.ondataavailable = (dataEvent: BlobEvent) => {
+    console.log('Recording data available');
+    if (recordingStream.onended) {
+      recordingStream.onended(dataEvent.data);
+    }
+  };
+  recorder.onstop = () => {
+    console.log('Recording stopped');
+  };
+  return new Promise<RecordingStream>((resolve, reject) => {
+    recorder.onstart = () => {
+      console.log('Recording started');
+      resolve(recordingStream);
+    };
+    recorder.onerror = (ev) => {
+      console.warn('Recording error', ev.error);
+      reject(ev.error);
+    };
+    recorder.start();
+  });
+}
+
+async function startAudioContextRecording(stream: MediaStream, bufferSize: number): Promise<RecordingStream> {
+  const audioContextType = (window as any).AudioContext || (window as any).webkitAudioContext;
+  if (!audioContextType) {
+    return Promise.reject(new Error('AudioContext not supported'));
+  }
+  const context = new audioContextType();
+  const sampleRate = context.sampleRate;
+  const streamNode = context.createMediaStreamSource(stream);
+  const processorNode = context.createScriptProcessor(bufferSize, 1, 1);
+  const recordingStream: RecordingStream = {
+    onended: null,
+    stop: () => {
+      if (stream) {
+        stream.getAudioTracks()[0].stop();
+      }
+      if (recordingStream.onended) {
+        recordingStream.onended(audioBuffersToWAV(buffers, sampleRate));
+      }
+    }
+  };
+  const buffers: Float32Array[] = [];
+  streamNode.connect(processorNode);
+  processorNode.connect(context.destination);
+  stream.getAudioTracks()[0].onended = () => {
+    streamNode.disconnect();
+    processorNode.disconnect();
+    if (recordingStream.onended) {
+      recordingStream.onended(audioBuffersToWAV(buffers, sampleRate));
+    }
+  };
+  processorNode.addEventListener('audioprocess', (ev: AudioProcessingEvent) => {
+    buffers.push(ev.inputBuffer.getChannelData(0));
+  });
+  return recordingStream;
+}
+
+function audioBuffersToWAV(buffers: Float32Array[], sampleRate: number): Blob {
+  let sampleCount = 0;
+  for (const b of buffers) {
+    sampleCount += b.length;
+  }
+  const buffer = new ArrayBuffer(44 + sampleCount * 2);
+  const view = new DataView(buffer);
+  writeWAVString(view, 0, 'RIFF');
+  // file length
+  view.setUint32(4, 32 + sampleCount * 2, true);
+  writeWAVString(view, 8, 'WAVE');
+  writeWAVString(view, 12, 'fmt ');
+  // chunk length
+  view.setUint32(16, 16, true);
+  // sample format
+  view.setUint16(20, 1, true);
+  // channel count
+  view.setUint16(22, 1, true);
+  // sample rate
+  view.setUint32(24, sampleRate, true);
+  // byte rate
+  view.setUint32(28, sampleRate * 4, true);
+  // block align
+  view.setUint16(32, 4, true);
+  // bits per sample
+  view.setUint16(34, 16, true);
+  // data chunk identifier
+  writeWAVString(view, 36, 'data');
+  // data length
+  view.setUint32(40, sampleCount * 2, true);
+  // data
+  let offset = 44;
+  for (const b of buffers) {
+    for (const spl of b) {
+      const s = Math.max(-1, Math.min(1, spl));
+      view.setInt16(offset, s < 0 ? s * 0x8000 : s * 0x7FFF, true);
+      offset += 2;
+    }
+  }
+  return new Blob([buffer], { type: 'audio/wav' });
+}
+
+function writeWAVString(view: DataView, offset: number, str: string) {
+  for (let i = 0; i < str.length; i++) {
+    view.setUint8(offset + i, str.charCodeAt(i));
+  }
 }
 
 export function play(buffer: Blob): Promise<PlaybackStream> {
