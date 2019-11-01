482866bf4361c6eb1758de8df9727d5928ae21bd
Aaron Neugebauer <aaron.n@rushdigital.co.nz>
2019-11-01 17:25:28 +1300

    Started moving add word fields to component.

--- client/src/pages/add-word/add-word.ts
+++ client/src/pages/add-word/add-word.ts
@@ -1,4 +1,4 @@
-import { AfterViewInit, Component, Inject, InjectionToken, NgZone } from '@angular/core';
+import { AfterViewInit, Component, Inject, NgZone, ViewChild } from '@angular/core';
 import { Location } from '@angular/common';
 import { Router } from '@angular/router';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
@@ -7,32 +7,11 @@ import { ErrorPopUpComponent } from 'components/error-popup/error-popup';
 import { ANALYTICS_SERVICE, IAnalyticsService } from 'services/analytics';
 import { FEEDBACK_SERVICE, IFeedbackService } from 'services/feedback';
 import { LoadingPopUpComponent } from 'components/loading-popup/loading-popup';
-import { startRecording, play, AudioStream, RecordingStream } from 'util/audio';
-import { OperatingSystem, getOperatingSystem } from 'util/platform';
 import { WordTranslation } from 'services/entities/translation';
 import { I18n } from '@ngx-translate/i18n-polyfill';
 import { environment } from 'environments/environment';
 import { AddedWord } from 'services/entities/feedback';
-
-enum RecordingState {
-  Idle,
-  Recording,
-  Finished,
-  Playing
-}
-
-interface AddWordConfig {
-  endangeredLanguage: string,
-  maxRecordingDuration: number;
-  recordingBufferSize: number;
-  recordingMimeTypes: string[];
-  androidGBoardUrl: string;
-  iosGBoardUrl: string;
-  keymanUrl: string;
-  progressAnimationInterval: number;
-}
-
-export const ADD_WORD_CONFIG = new InjectionToken<AddWordConfig>('Add Word config');
+import { AddWordFieldsetComponent } from 'components/add-word-fieldset/add-word-fieldset';
 
 @Component({
   selector: 'app-page-add-word',
@@ -41,22 +20,14 @@ export const ADD_WORD_CONFIG = new InjectionToken<AddWordConfig>('Add Word confi
 })
 export class AddWordPageComponent implements AfterViewInit {
   public readonly form: FormGroup;
-  private audioStream: AudioStream|null = null;
-  private recording: Blob|null = null;
   private readonly prevPageCssClass?: string;
-  public audioStreamProgress = 0;
   public submittingForm = false;
-  public recordingState = RecordingState.Idle;
-  public recordingStateValues = RecordingState;
-  public operatingSystem: OperatingSystem;
-  public operatingSystemValues = OperatingSystem;
-  public gboardUrl: string;
-  public keymanUrl: string;
+  @ViewChild('addWordFieldset', { static: true })
+  private addWordFieldset: AddWordFieldsetComponent|null = null;
 
   public get endangeredLanguage(): string { return environment.endangeredLanguage; }
 
-  constructor( @Inject(ADD_WORD_CONFIG) private config: AddWordConfig,
-               private router: Router,
+  constructor( private router: Router,
                private location: Location,
                private dialog: MatDialog,
                private snackBar: MatSnackBar,
@@ -74,13 +45,9 @@ export class AddWordPageComponent implements AfterViewInit {
       ]),
       transliteration: new FormControl(word ? word.transliteration : '', [
       ]),
-      pronunciation: new FormControl(null, [
+      recording: new FormControl(null, [
       ]),
     });
-    this.operatingSystem = getOperatingSystem();
-    this.keymanUrl = this.config.keymanUrl;
-    this.gboardUrl = this.operatingSystem === OperatingSystem.Android ?
-      this.config.androidGBoardUrl : this.config.iosGBoardUrl;
     this.prevPageCssClass = history.state.prevPageCssClass;
   }
 
@@ -89,14 +56,13 @@ export class AddWordPageComponent implements AfterViewInit {
   }
 
   onFormSubmit() {
+    this.form.markAsDirty( {onlySelf: false} );
     if (!this.form.valid) {
       return;
     }
     this.submittingForm = true;
     const loadingPopup = this.dialog.open(LoadingPopUpComponent, { panelClass: 'loading-popup' });
-    const addedWord: AddedWord = this.form.value;
-    addedWord.recording = this.recording;
-    this.feedbackService.addWord(addedWord).then(
+    this.feedbackService.addWord(this.form.value).then(
       () => {
         console.log('Added word submitted');
         this.location.back();
@@ -120,88 +86,5 @@ export class AddWordPageComponent implements AfterViewInit {
   onCloseClick(ev: Event) {
     ev.preventDefault();
     history.back();
-  }
-
-  onStartRecordingClick() {
-    console.log('Starting recording');
-    this.audioStreamProgress = 0;
-    this.recordingState = RecordingState.Recording;
-    startRecording(this.config.recordingBufferSize, this.config.recordingMimeTypes).then(
-      this.onRecordingStarted,
-      (err) => {
-        console.warn('Error starting recording', err);
-        this.zone.run(() => {
-          this.recordingState = RecordingState.Idle;
-        });
-      }
-    );
-  }
-
-  onRecordingStarted = (stream: RecordingStream) => {
-    console.log('Recording started');
-    this.audioStream = stream;
-    const recordingTimeout = setTimeout(() => {
-      stream.stop();
-    }, this.config.maxRecordingDuration);
-    const startTime = Date.now();
-    const progressInterval = setInterval(() => {
-      this.zone.run(() => {
-        this.audioStreamProgress = (Date.now() - startTime) / this.config.maxRecordingDuration;
-      });
-    }, this.config.progressAnimationInterval);
-    stream.onended = (buffer) => {
-      console.log('Recording complete');
-      clearTimeout(recordingTimeout);
-      clearInterval(progressInterval);
-      this.recording = buffer;
-      this.audioStream = null;
-      this.form.controls.pronunciation.setValue(buffer);
-      this.zone.run(() => {
-        this.recordingState = RecordingState.Finished;
-      });
-    };
-  };
-
-  onStopRecordingClick() {
-    console.log('Stopping audio');
-    if (this.audioStream) {
-      this.audioStream.stop();
-    }
-    this.recordingState = this.recording ? RecordingState.Finished : RecordingState.Idle;
-  }
-
-  onPlayRecordingClick() {
-    console.log('Starting playback');
-    if (!this.recording) {
-      console.warn('No audio recorded');
-      return false;
-    }
-    this.audioStreamProgress = 0;
-    this.recordingState = RecordingState.Playing;
-    play(this.recording).then(
-      (stream) => {
-        const duration = Number.isFinite(stream.getDuration()) ? stream.getDuration() : this.config.maxRecordingDuration * 0.001;
-        console.log(duration);
-        this.audioStream = stream;
-        const progressInterval = setInterval(() => {
-          this.zone.run(() => {
-            this.audioStreamProgress = stream.getCurrentTime() / duration;
-          });
-        }, this.config.progressAnimationInterval);
-        stream.onended = () => {
-          console.log('Playback ended');
-          clearInterval(progressInterval);
-          this.zone.run(() => {
-            this.recordingState = RecordingState.Finished;
-          });
-        };
-      },
-      (err) => {
-        console.warn('Error playing recording', err);
-        this.zone.run(() => {
-          this.recordingState = RecordingState.Finished;
-        });
-      }
-    );
   }
 }
