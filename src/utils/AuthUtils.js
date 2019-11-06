2450e6d9bb47716edaf641a1cd2926ec4e6f13fd
Michele Chiappalone <chiappalone@google.com>
2019-11-06 18:14:10 +0000

    Addiing deploy functions

--- src/utils/AuthUtils.js
+++ src/utils/AuthUtils.js
@@ -1,16 +1,7 @@
 import firebase from 'firebase/app';
 import 'firebase/auth';
 
-const firebaseConfig_ = {
-  apiKey: "AIzaSyAdh6AhIjdLezKR0SH3x6vzbMh1_benKk8",
-  authDomain: "barnard-project.firebaseapp.com",
-  databaseURL: "https://barnard-project.firebaseio.com",
-  projectId: "barnard-project",
-  storageBucket: "barnard-project.appspot.com",
-  messagingSenderId: "929114075380",
-  appId: "1:929114075380:web:3ab89bf036919331262ea1",
-  measurementId: "G-4X4KQ1ZF68"
-};
+const firebaseConfig_ = _CONFIG_PLACEHOLDER_;
 
 class AuthUtils {
   constructor() {
