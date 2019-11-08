9725c6dfb3066705284af92edbf8d69306311ecb
Shiv Parikh <parikhshiv@google.com>
2019-11-08 14:01:54 -0800

    use sample project api key locally

--- src/utils/AuthUtils.js
+++ src/utils/AuthUtils.js
@@ -1,7 +1,20 @@
 import firebase from 'firebase/app';
 import 'firebase/auth';
 
-const firebaseConfig_ = _CONFIG_PLACEHOLDER_;
+const IS_LOCAL = process.env.NODE_ENV === 'development';
+
+const localFirebaseConfig_ = {
+  apiKey: "AIzaSyAdh6AhIjdLezKR0SH3x6vzbMh1_benKk8",
+  authDomain: "barnard-project.firebaseapp.com",
+  databaseURL: "https://barnard-project.firebaseio.com",
+  projectId: "barnard-project",
+  storageBucket: "barnard-project.appspot.com",
+  messagingSenderId: "929114075380",
+  appId: "1:929114075380:web:3ab89bf036919331262ea1",
+  measurementId: "G-4X4KQ1ZF68"
+};
+
+const firebaseConfig_ = IS_LOCAL ? localFirebaseConfig_ : _CONFIG_PLACEHOLDER_; // eslint-disable-line
 
 class AuthUtils {
   constructor() {
