c3cb1b1489fcd304f19b81d3614be0248c87ff88
Aaron Neugebauer <aaron.n@rushdigital.co.nz>
2019-11-12 17:10:47 +1300

    Fixed image orientation issues.

--- client/src/pages/photo-source/photo-source.ts
+++ client/src/pages/photo-source/photo-source.ts
@@ -7,57 +7,24 @@ import { LoadingPopUpComponent } from 'components/loading-popup/loading-popup';
 import { AppRoutes } from 'app/routes';
 import { SessionService } from 'services/session';
 import { addOpenedListener } from 'util/dialog';
+import { ImageLoaderPageBase } from 'pages/capture/capture';
 
 @Component({
   selector: 'app-page-photo-source',
   templateUrl: './photo-source.html',
   styleUrls: ['./photo-source.scss']
 })
-export class PhotoSourcePageComponent implements AfterViewInit {
-  constructor( private router: Router,
-               private dialog: MatDialog,
-               private sessionService: SessionService,
-               @Inject(ANALYTICS_SERVICE) private analyticsService: IAnalyticsService,
-               @Inject(IMAGE_RECOGNITION_SERVICE) private imageRecognitionService: IImageRecognitionService ) {
+export class PhotoSourcePageComponent extends ImageLoaderPageBase implements AfterViewInit {
+  constructor( router: Router,
+               dialog: MatDialog,
+               sessionService: SessionService,
+               @Inject(IMAGE_RECOGNITION_SERVICE) imageRecognitionService: IImageRecognitionService,
+               @Inject(ANALYTICS_SERVICE) private analyticsService: IAnalyticsService) {
+    super(router, dialog, sessionService, imageRecognitionService);
   }
 
   ngAfterViewInit() {
     this.analyticsService.logPageView(this.router.url, 'Photo Source');
-  }
-
-  onImageUploaded(image: Blob) {
-    const loadingPopUp = this.dialog.open(LoadingPopUpComponent,
-      { closeOnNavigation: false, disableClose: true, panelClass: 'loading-popup' });
-    this.sessionService.currentSession.currentModal = loadingPopUp;
-    loadingPopUp.beforeClosed().subscribe({
-      complete: () => this.sessionService.currentSession.currentModal = null
-    });
-    addOpenedListener(loadingPopUp, () => {
-      this.imageRecognitionService.loadDescriptions(image).then(
-        (descriptions) => {
-          if (descriptions.length > 0) {
-            this.router.navigateByUrl(AppRoutes.Translate, { state: { image, words: descriptions.map(d => d.description) } }).then(
-              (success) => {
-                if (!success) {
-                  loadingPopUp.close();
-                }
-              },
-              () => loadingPopUp.close()
-            );
-          } else {
-            this.router.navigateByUrl(AppRoutes.CaptionImage, { state: { image } }).finally(
-              () => loadingPopUp.close()
-            );
-          }
-        },
-        (err) => {
-          console.warn('Error loading image descriptions', err);
-          this.router.navigateByUrl(AppRoutes.CaptionImage, { state: { image } }).finally(
-            () => loadingPopUp.close()
-          );
-        }
-      );
-    });
   }
 
   onCaptureClick() {
