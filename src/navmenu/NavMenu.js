a1aeb2931f08aa6e3c3d04cb65735e3e42d804cf
Lien Hoang <kimlienhg@gmail.com>
2019-11-16 19:37:13 +0800

    Make navigation menu responsive

--- src/navmenu/NavMenu.js
+++ src/navmenu/NavMenu.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { slide as SlideMenu } from 'react-burger-menu'
 import { withRouter } from 'react-router-dom';
 import Paper from '@material-ui/core/Paper';
+import MenuList from '@material-ui/core/MenuList';
 import MenuItem from '@material-ui/core/MenuItem';
 import './NavMenu.css';
 import { ROUTES } from '../App';
@@ -11,16 +12,14 @@ const TRANSLATIONS_LABEL = "Translations";
 class NavMenu extends React.Component {
   constructor(props) {
     super(props);
-
-    this.handleStateChange = this.handleStateChange.bind(this);
-
-    this.state = {
-      menuOpen: false,
-    }
   }
+
   onClick(route) {
-    this.setState({menuOpen: false});
     this.props.history.push(route);
+    // Close the side menu (only applicable to mobile widths).
+    if (this.props.closeSideMenu) {
+      this.props.closeSideMenu();
+    }
   }
 
   renderMenuItem(label, route) {
@@ -42,22 +41,16 @@ class NavMenu extends React.Component {
     );
   }
 
-  handleStateChange(state) {
-    this.setState({menuOpen: state.menuOpen});
-  }
-
   render() {
     return (
-      <SlideMenu className="slide-menu" isOpen={this.state.menuOpen}
-        onStateChange={this.handleStateChange}
-      >
+      <MenuList>
         {this.renderMenuItem("Add Words", ROUTES.ADD_WORDS)}
         {this.renderMenuItem(TRANSLATIONS_LABEL, ROUTES.TRANSLATIONS)}
         {this.renderMenuItem("User Contributions", ROUTES.CONTRIBUTIONS)}
         {this.renderMenuItem("Theme Customization", ROUTES.THEME)}
         {this.renderMenuItem("Sharing Links", ROUTES.SHARING)}
         {this.renderMenuItem("User Management", ROUTES.MANAGEMENT)}
-      </SlideMenu>
+      </MenuList>
     );
   }
 }
