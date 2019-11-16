7b264af3c9100282afaf9589c7219f5eba3f3670
Lien Hoang <kimlienhg@gmail.com>
2019-11-16 19:37:13 +0800

    Make navigation menu responsive

---
+++ src/navmenu/HamburgerNavMenu.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import { slide as SlideMenu } from 'react-burger-menu'
+import NavMenu from './NavMenu'
+import './HamburgerNavMenu.css';
+
+/** 
+  * Wrapper around NavMenu, used for mobile widths. This renders
+  * a hamburger icon that controls a sliding menu.
+  */
+class HamburgerNavMenu extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.handleStateChange = this.handleStateChange.bind(this);
+
+    this.state = {
+      menuOpen: false,
+    }
+  }
+
+  closeMenu() {
+    this.setState({menuOpen: false});
+  }
+
+  handleStateChange(state) {
+    this.setState({menuOpen: state.menuOpen});
+  }
+
+  render() {
+    return (
+      <SlideMenu className="slide-menu" isOpen={this.state.menuOpen}
+        onStateChange={this.handleStateChange}
+      >
+        <NavMenu closeSideMenu={this.closeMenu.bind(this)} />
+      </SlideMenu>
+    );
+  }
+}
+
+export default HamburgerNavMenu;
