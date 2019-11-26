4cafb0e7c5d8a8d6e47c46a718fb3c110b340c05
Yiling Liu <lyleaf@gmail.com>
2019-11-27 09:49:11 +1100

    add approvedSuggestion

--- src/utils/ApiUtils.js
+++ src/utils/ApiUtils.js
@@ -3,6 +3,6 @@ const IS_LOCAL = process.env.NODE_ENV === 'development'
 
 export default {
   origin: IS_LOCAL ? 'http://localhost:5000' :
-    'https://us-central1-barnard-project.cloudfunctions.net',
+    'https://us-central1-final-test-woolaroo.cloudfunctions.net',
   path: IS_LOCAL ? '/barnard-project/us-central1/' : '/',
 }
