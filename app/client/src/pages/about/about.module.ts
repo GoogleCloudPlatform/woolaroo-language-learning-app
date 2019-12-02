980b3039058d5cc5cb1f456ea4fa512c2492e89c
Aaron Neugebauer <aaron.n@rushdigital.co.nz>
2019-12-03 11:27:49 +1300

    Fixed Arts and Culture logo URL.

--- app/client/src/pages/about/about.module.ts
+++ app/client/src/pages/about/about.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { MatToolbarModule } from '@angular/material';
 import { AboutPageComponent } from './about';
 import { IconComponentModule } from 'components/icon/icon.module';
+import { PipesModule } from 'pipes/pipes.module';
 
 @NgModule({
   declarations: [
     AboutPageComponent,
   ],
   imports: [
+    PipesModule,
     MatToolbarModule,
     IconComponentModule
   ]
