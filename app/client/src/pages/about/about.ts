87158cebd96c6a853c4d9bf5dae282fa3254733e
Aaron Neugebauer <aaron.n@rushdigital.co.nz>
2020-02-21 15:34:04 +1300

    Updated copy and translations.

--- app/client/src/pages/about/about.ts
+++ app/client/src/pages/about/about.ts
@@ -1,7 +1,6 @@
 import { AfterViewInit, Component, Inject } from '@angular/core';
 import { Router } from '@angular/router';
 import { IAnalyticsService, ANALYTICS_SERVICE } from 'services/analytics';
-import { environment } from 'environments/environment';
 
 @Component({
   selector: 'app-page-about',
@@ -9,8 +8,6 @@ import { environment } from 'environments/environment';
   styleUrls: ['./about.scss']
 })
 export class AboutPageComponent implements AfterViewInit {
-  public get endangeredLanguage(): string { return environment.endangeredLanguage; }
-
   constructor( private router: Router,
                @Inject(ANALYTICS_SERVICE) private analyticsService: IAnalyticsService ) {
   }
