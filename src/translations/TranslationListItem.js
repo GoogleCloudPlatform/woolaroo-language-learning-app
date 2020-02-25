1835b6e08799be85ac92ae213d5f3429feb4d603
Jason Bakthakumar <jasonbakthakumar@A1953-MacBook-Pro.local>
2020-02-26 10:16:16 +1300

    translate page is now responsive

--- src/translations/TranslationListItem.js
+++ src/translations/TranslationListItem.js
@@ -1,11 +1,11 @@
-import React from 'react';
-import Button from '@material-ui/core/Button';
-import ListItemBase from '../common/ListItemBase'
-import ApiUtils from '../utils/ApiUtils';
-import AuthUtils from '../utils/AuthUtils';
-import DeleteIcon from '@material-ui/icons/Delete';
-import IconButton from '@material-ui/core/IconButton';
-import './TranslationListItem.css';
+import React from "react";
+import Button from "@material-ui/core/Button";
+import ListItemBase from "../common/ListItemBase";
+import ApiUtils from "../utils/ApiUtils";
+import AuthUtils from "../utils/AuthUtils";
+import DeleteIcon from "@material-ui/icons/Delete";
+import IconButton from "@material-ui/core/IconButton";
+import "./TranslationListItem.scss";
 
 class TranslationListItem extends ListItemBase {
   constructor(props) {
@@ -22,156 +22,170 @@ class TranslationListItem extends ListItemBase {
 
     this.state = {
       ...this.state,
-      collectionName: 'translations',
+      collectionName: "translations",
       disabled: true,
-      error: false,
+      error: false
     };
   }
 
-  handleTranslationChange = (e) => {
+  handleTranslationChange = e => {
     const newTranslation = e.target.value;
     this.setState({
       translation: newTranslation,
-      disabled: newTranslation === this.savedData.translation,
+      disabled: newTranslation === this.savedData.translation
     });
-  }
+  };
 
-  handleTransliterationChange = (e) => {
+  handleTransliterationChange = e => {
     const newTransliteration = e.target.value;
     this.setState({
       transliteration: newTransliteration,
-      disabled: newTransliteration === this.savedData.transliteration,
+      disabled: newTransliteration === this.savedData.transliteration
     });
-  }
+  };
 
-  saveTranslation_ = async (e) => {
+  saveTranslation_ = async e => {
     // TODO(smus): While the translation is being saved, show a progress bar and
     // prevent the button from being clicked again.
-    await this.setStateAsync({disabled: true});
+    await this.setStateAsync({ disabled: true });
 
     // First, if there's a new sound_blob, we should upload it and update our
     // sound_link.
-    const {sound_blob} = this.state;
+    const { sound_blob } = this.state;
     if (sound_blob) {
       const sound_link = await this.saveSoundBlob_(sound_blob);
-      await this.setStateAsync({sound_link});
+      await this.setStateAsync({ sound_link });
     }
     // Then, call endpoint to update the translation.
     try {
-      let { english_word, primary_word, sound_link, translation,
-        transliteration, frequency } = this.state;
-      english_word = (""+english_word).trim();
-      primary_word = (""+primary_word).trim();
-      translation = (""+translation).trim();
-      transliteration = (""+transliteration).trim();
-      
+      let {
+        english_word,
+        primary_word,
+        sound_link,
+        translation,
+        transliteration,
+        frequency
+      } = this.state;
+      english_word = ("" + english_word).trim();
+      primary_word = ("" + primary_word).trim();
+      translation = ("" + translation).trim();
+      transliteration = ("" + transliteration).trim();
+
       // If we have no data for this entry, show an error.
       if (!sound_link && !translation && !transliteration) {
         this.setState({
           // TODO(parikshiv) - add visible error state, also figure out if any
           // of these can be empty?
-          error: true,
-        })
+          error: true
+        });
         return;
       }
 
       // Save any updated data for the translation entry.
-      const resp = await fetch(`${ApiUtils.origin}${ApiUtils.path}addTranslations`, {
-        method: 'POST',
-        body: JSON.stringify({
-          english_word,
-          primary_word,
-          sound_link,
-          translation,
-          transliteration,
-          frequency,
-        }),
-        headers: {
-          'Authorization': await AuthUtils.getAuthHeader(),
-          'Content-Type': 'application/json',
+      const resp = await fetch(
+        `${ApiUtils.origin}${ApiUtils.path}addTranslations`,
+        {
+          method: "POST",
+          body: JSON.stringify({
+            english_word,
+            primary_word,
+            sound_link,
+            translation,
+            transliteration,
+            frequency
+          }),
+          headers: {
+            Authorization: await AuthUtils.getAuthHeader(),
+            "Content-Type": "application/json"
+          }
         }
-      });
+      );
       if (resp.status === 200) {
-        await this.showPopup('Saved!');
+        await this.showPopup("Saved!");
         this.savedData = {
           translation,
           transliteration
         };
-        
+
         this.setState({
-          disabled: true,
-        });  
+          disabled: true
+        });
         await this.actionaftersaving();
       } else {
-        await this.showPopup('Failed to save. Please try again!');
+        await this.showPopup("Failed to save. Please try again!");
         console.errpr(resp.text());
-        await this.setStateAsync({disabled: false});
+        await this.setStateAsync({ disabled: false });
       }
-      
-      
-    } catch(err) {
-      await this.showPopup('Failed to save. Please try again!');
+    } catch (err) {
+      await this.showPopup("Failed to save. Please try again!");
       console.error(err);
     }
-  }
+  };
 
   /** Saves audio and returns the URL. */
-  saveSoundBlob_ = async (blob) => {
+  saveSoundBlob_ = async blob => {
     try {
       const base64Audio = await blobToBase64(blob);
-      const res = await fetch(`${ApiUtils.origin}${ApiUtils.path}saveAudioSuggestions`, {
-        method: 'POST',
-        body: base64Audio,
-        headers: {
-          'Authorization': await AuthUtils.getAuthHeader(),
+      const res = await fetch(
+        `${ApiUtils.origin}${ApiUtils.path}saveAudioSuggestions`,
+        {
+          method: "POST",
+          body: base64Audio,
+          headers: {
+            Authorization: await AuthUtils.getAuthHeader()
+          }
         }
-      });
+      );
       if (res.status === 403) {
-        await this.showPopup('Failed to save. Please try again!');
+        await this.showPopup("Failed to save. Please try again!");
         console.error(res.text());
         return;
       }
       return await res.text();
-    } catch(err) {
-      await this.showPopup('Failed to save. Please try again!');
+    } catch (err) {
+      await this.showPopup("Failed to save. Please try again!");
       console.error(err);
     }
-  }
-  actionaftersaving(){
+  };
+  actionaftersaving() {
     //do nothing. to be overwritten in other functions to do things like remove the row
     return;
   }
   renderEndOfRow() {
     const endOfRow = [
-      <Button
-        variant="contained"
-        color="primary"
-        disabled={this.state.disabled}
-        onClick={this.saveTranslation_}
-        key={0}
-        className="save-button"
-      >
-        Save
-      </Button>
+      <div className="save-button-container">
+        <Button
+          variant="contained"
+          color="primary"
+          disabled={this.state.disabled}
+          onClick={this.saveTranslation_}
+          key={0}
+          className="save-button"
+        >
+          Save
+        </Button>
+      </div>
     ];
 
     endOfRow.push(
-      <IconButton
-        aria-label="delete"
-        className="delete-translation"
-        key={1}
-        onClick={this.showDeleteConfirm_}
-      >
-        <DeleteIcon />
-      </IconButton>
+      <div className="delete-button-container">
+        <IconButton
+          aria-label="delete"
+          className="delete-translation"
+          key={1}
+          onClick={this.showDeleteConfirm_}
+        >
+          <DeleteIcon />
+        </IconButton>
+      </div>
     );
 
     return endOfRow;
   }
 
   async setStateAsync(state) {
-    return new Promise((resolve) => {
-      this.setState(state, resolve)
+    return new Promise(resolve => {
+      this.setState(state, resolve);
     });
   }
 }
@@ -183,9 +197,9 @@ function blobToBase64(blob) {
     reader.onloadend = () => {
       const base64 = reader.result;
       // Cut off the data:audio/webm part.
-      const base64Data = base64.split(',')[1];
+      const base64Data = base64.split(",")[1];
       resolve(base64Data);
-    }
+    };
     reader.onerror = reject;
   });
 }
