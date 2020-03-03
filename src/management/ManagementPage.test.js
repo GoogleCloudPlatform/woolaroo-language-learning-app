17ec8e5e8863aaf374493804732e3e0f7d136583
Jason Bakthakumar <jasonbakthakumar@A1953-MacBook-Pro.local>
2020-03-04 10:51:16 +1300

    responsive tables

--- src/management/ManagementPage.test.js
+++ src/management/ManagementPage.test.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import ManagementPage from './ManagementPage';
+import { Breakpoint } from "react-socks";
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<ManagementPage />, div);
+  ReactDOM.render(<Breakpoint><ManagementPage /></Breakpoint>, div);
   ReactDOM.unmountComponentAtNode(div);
 });
