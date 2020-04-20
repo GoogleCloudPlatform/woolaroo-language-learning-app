af38782a6ec9632ae763aee4a9c86782716308fe
Aaron Neugebauer <aaron.n@rushdigital.co.nz>
2020-04-21 11:39:37 +1200

    Build fixes.

--- app/scripts/gcp_setup.sh
+++ app/scripts/gcp_setup.sh
@@ -10,18 +10,17 @@ TERRAFORM_BUCKET_NAME=${PROJECT_ID}-terraform
 # Enable required APIs
 gcloud services enable cloudbuild.googleapis.com
 gcloud services enable appengine.googleapis.com
-gcloud services enable vision.googleapis.com
 
 # Get build service account by role
 BUILD_SERVICE_ACCOUNT=`gcloud projects get-iam-policy ${PROJECT_ID} --filter=bindings.role:roles/cloudbuild.builds.builder --format='table[no-heading](bindings.members)' --flatten='bindings[].members' --limit=1`
 
 # Create custom role for build service
-gcloud iam roles create barnard.appdeployer --project=${PROJECT_ID} --permissions=appengine.applications.create
+gcloud iam roles create woolaroo.appdeployer --project=${PROJECT_ID} --permissions=appengine.applications.create
 
 # Add required roles to build service account
 gcloud projects add-iam-policy-binding ${PROJECT_ID} --member ${BUILD_SERVICE_ACCOUNT} --role roles/storage.admin
 gcloud projects add-iam-policy-binding ${PROJECT_ID} --member ${BUILD_SERVICE_ACCOUNT} --role roles/appengine.appAdmin
-gcloud projects add-iam-policy-binding ${PROJECT_ID} --member ${BUILD_SERVICE_ACCOUNT} --role projects/${PROJECT_ID}/roles/barnard.appdeployer
+gcloud projects add-iam-policy-binding ${PROJECT_ID} --member ${BUILD_SERVICE_ACCOUNT} --role projects/${PROJECT_ID}/roles/woolaroo.appdeployer
 
 # Create a bucket for terraform state
 gsutil mb -c nearline -l ${TERRAFORM_BUCKET_LOCATION} -b on gs://${TERRAFORM_BUCKET_NAME}
---
+++ multilanguage-functions/gcp_setup.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+# Google project ID - default to current project ID
+PROJECT_ID=`gcloud config get-value project`
+
+# Enable required APIs
+gcloud services enable firebase.googleapis.com
+gcloud services enable firestore.googleapis.com
+gcloud services enable cloudresourcemanager.googleapis.com
+gcloud services enable cloudbuild.googleapis.com
+gcloud services enable cloudkms.googleapis.com
+gcloud services enable vision.googleapis.com
+
+# Get build service account by role
+BUILD_SERVICE_ACCOUNT=`gcloud projects get-iam-policy ${PROJECT_ID} --filter=bindings.role:roles/cloudbuild.builds.builder --format='table[no-heading](bindings.members)' --flatten='bindings[].members' --limit=1`
+
+# Add required roles to build service account
+gcloud projects add-iam-policy-binding ${PROJECT_ID} --member ${BUILD_SERVICE_ACCOUNT} --role roles/cloudfunctions.developer
+gcloud projects add-iam-policy-binding ${PROJECT_ID} --member ${BUILD_SERVICE_ACCOUNT} --role roles/cloudkms.cryptoKeyDecrypter
+gcloud projects add-iam-policy-binding ${PROJECT_ID} --member ${BUILD_SERVICE_ACCOUNT} --role roles/firebase.admin
+gcloud projects add-iam-policy-binding ${PROJECT_ID} --member ${BUILD_SERVICE_ACCOUNT} --role roles/iam.serviceAccountUser
+gcloud projects add-iam-policy-binding ${PROJECT_ID} --member ${BUILD_SERVICE_ACCOUNT} --role roles/serviceusage.apiKeysAdmin
