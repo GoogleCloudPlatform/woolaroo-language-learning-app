433d0846ec229eb988571a750f3167d468c809ad
Aaron Neugebauer <aaron.n@rushdigital.co.nz>
2020-04-21 17:37:18 +1200

    Fixed uuid call.

--- multilanguage-functions/index.js
+++ multilanguage-functions/index.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 const functions = require('firebase-functions');
-const {uuidv1} = require('uuid');
+const uuidv1 = require('uuid/uuidv1');
 const admin = require('firebase-admin');
 const cors = require('cors')({origin: true});
 const vision = require('@google-cloud/vision');
