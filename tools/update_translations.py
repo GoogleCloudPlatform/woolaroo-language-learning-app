f6598a1a5ddbb22af5f88815833a17a1ba5921b1
Aaron Neugebauer <aaron.n@rushdigital.co.nz>
2020-09-23 23:20:21 +1200

    Fixed word formatting. Batched requests.

--- tools/update_translations.py
+++ tools/update_translations.py
@@ -1,6 +1,25 @@
 import csv
+from html.parser import HTMLParser
+
 from google.cloud import firestore
 import google.oauth2.credentials
+
+class HTMLStripper(HTMLParser):
+	def __init__(self):
+		super().__init__()
+		self.text = ''
+
+	def feed(self, *args, **kwargs):
+		self.text = ''
+		return super().feed(*args, **kwargs)
+
+	def handle_data(self, d):
+		self.text += d
+	
+	def get_data(self):
+		return self.text
+
+html_stripper = HTMLStripper()
 
 words = {}
 with open('./EndangeredLanguageTranslations.csv', encoding='utf-8') as translations_file:
@@ -11,18 +30,25 @@ with open('./EndangeredLanguageTranslations.csv', encoding='utf-8') as translati
 		language = row[0]
 		if not language:
 			continue
-		english_word = row[1].lower()
+		english_word = row[1].lower().strip()
 		if not english_word:
 			continue
+		html_stripper.feed(english_word)
+		english_word = html_stripper.get_data()
+		english_word = english_word.split('/')[0]
 		translation = row[2] if len(row) > 2 else None
 		transliteration = row[3] if len(row) > 3 else None
 		if not translation:
 			if not transliteration:
 				continue
 			translation = transliteration
-		translation = translation.lower()
+		translation = translation.lower().strip()
+		html_stripper.feed(translation)
+		translation = html_stripper.get_data()
 		if transliteration:
-			transliteration = transliteration.lower()
+			transliteration = transliteration.lower().strip()
+			html_stripper.feed(transliteration)
+			transliteration = html_stripper.get_data()
 		if english_word not in words:
 			word = {'en': {'sound_link': '', 'translation': english_word, 'transliteration': english_word}}
 			words[english_word] = word
@@ -34,11 +60,26 @@ with open('./EndangeredLanguageTranslations.csv', encoding='utf-8') as translati
 credentials = google.oauth2.service_account.Credentials.from_service_account_file(filename='google_api_key.json')
 db = firestore.Client(project='ggl-woolaroo-multilang-uat', credentials=credentials)
 collection = db.collection(u'translations')
+batch = firestore.WriteBatch(db)
 
 print('Deleting documents')
+count = 1
 for doc in collection.list_documents():
-	doc.delete()
+	batch.delete(doc)
+	if count % 500 == 0:
+		batch.commit()
+	if count % 100 == 0:
+		print(f'{count} deleted')
+	count = count + 1
+batch.commit()
 
 print('Creating documents')
+count = 1
 for key, val in words.items():
-	collection.add(val, key)
+	batch.create(firestore.DocumentReference('translations', key, client=db), val)
+	if count % 500 == 0:
+		batch.commit()
+	if count % 100 == 0:
+		print(f'{count}/{len(words)} created')
+	count = count + 1
+batch.commit()
