6c86dc50c903447d942c99cbef2cf7daac8c65a0
Justin Grayston <jgrayston@google.com>
2022-07-16 15:26:40 +0100

    Create app,yanl template and reuse config_server.js

--- deploy/configure_server.js
+++ deploy/configure_server.js
@@ -3,13 +3,20 @@ const path = require('path');
 
 const APP_SERVICE = process.argv[2];
 const APP_YAML_PATH = process.argv[3];
-if(!APP_YAML_PATH) {
+const APP_CACHE_TIME = process.argv[4];
+
+if (!APP_YAML_PATH) {
     throw new Error('Config file path not set');
 }
 
-if(APP_SERVICE) {
+if (!APP_CACHE_TIME) {
+    throw new Error('App cache time is not set, should be 2d or similar');
+}
+
+if (APP_SERVICE) {
     const appYamlPath = path.join(process.cwd(), APP_YAML_PATH);
-    let appYamlContent = fs.readFileSync(appYamlPath, {encoding: 'utf-8'});
-    appYamlContent += `\n\nservice: ${APP_SERVICE}`;
+    let appYamlContent = fs.readFileSync(appYamlPath, { encoding: 'utf-8' });
+    appYamlContent = appYamlContent.replace('$SERVICE', APP_SERVICE);
+    appYamlContent = appYamlContent.replace('$CACHE', APP_CACHE_TIME);
     fs.writeFileSync(appYamlPath, appYamlContent);
 }
