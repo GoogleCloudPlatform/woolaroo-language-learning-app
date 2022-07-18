70d4d15e76523c5ed4c1e45921fbb84deab11dca
Justin Grayston <jgrayston@google.com>
2022-07-18 15:53:16 +0100

    Add a dispatch.yaml configuration for serving in woolaroo dir
    This commit updates the server configuration script and cloud build
    to set GAE to serve the contents at a subdirector level.
    
    This is because this is how we serve it at Google, and isn't a
    requirement. This means we can configure a staging environment
    to be identical to prod and catch any issues early.
    
    Relates to #179

--- deploy/configure_server.js
+++ deploy/configure_server.js
@@ -4,6 +4,10 @@ const path = require('path');
 const APP_SERVICE = process.argv[2];
 const APP_YAML_PATH = process.argv[3];
 const APP_CACHE_TIME = process.argv[4];
+const DISPATCH_YAML_PATH = process.argv[5];
+const PROD_URL = process.argv[6];
+const STAGING_URL = process.argv[7];
+
 
 if (!APP_YAML_PATH) {
     throw new Error('Config file path not set');
@@ -19,4 +23,16 @@ if (APP_SERVICE) {
     appYamlContent = appYamlContent.replace('$SERVICE', APP_SERVICE);
     appYamlContent = appYamlContent.replace('$CACHE', APP_CACHE_TIME);
     fs.writeFileSync(appYamlPath, appYamlContent);
+
+    // Prepare the dispach.yaml
+    const dispatchYamlPath = path.join(process.cwd(), DISPATCH_YAML_PATH);
+    let dispatchYamlContent = fs.readFileSync(
+        dispatchYamlPath, { encoding: 'utf-8' });
+
+    dispatchYamlContent = dispatchYamlContent.replace('{{PROD_URL}}', PROD_URL);
+    dispatchYamlContent = dispatchYamlContent
+        .replace('{{STAGING_URL}}', STAGING_URL);
+
+    fs.writeFileSync(dispatchYamlPath, dispatchYamlContent);
+
 }
