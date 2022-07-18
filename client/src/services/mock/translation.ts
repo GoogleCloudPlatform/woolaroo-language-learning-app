d4cd481f60cbcfed3ccc473cc8eff76d97a92943
Justin Grayston <jgrayston@google.com>
2022-07-18 12:00:10 +0100

    Update start script to place the app in the path /woolaroo
    This reflects how the application is deployed, and allows testing
    and development that reflects this. (CSS asset paths etc will
    break if this the case).

--- client/src/services/mock/translation.ts
+++ client/src/services/mock/translation.ts
@@ -5,12 +5,12 @@ import { ITranslationService } from '../translation';
 @Injectable()
 export class MockTranslationService implements ITranslationService {
   public async translate(words: string[], primaryLanguage: string, targetLanguage: string, maxTranslations: number = 0): Promise<WordTranslation[]> {
-    return words.map((w, index) =>({
+    return words.map((w, index) => ({
       original: w,
       english: w + ' en',
       translation: index > 0 ? w + ' tr' : '白天',
       transliteration: '白天',
-      soundURL: index > 1 ? '/assets/debug/translation.mp3' : null
+      soundURL: index > 1 ? '/assets/debug/translation.mp3?v=1' : null
     }));
   }
 }
