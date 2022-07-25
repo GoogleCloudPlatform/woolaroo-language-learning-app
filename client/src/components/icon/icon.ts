53d0f0ff64c781265887536f01787db247fa50bb
Justin Grayston <jgrayston@google.com>
2022-07-25 21:27:04 +0100

    Fix form button dropshadow
    Also fixes
    
    - mat-body-1 content styling in technology and about pages
    - Fixes mat-toolbar-single-row dropshadow.
    
    Closes #197

--- client/src/components/icon/icon.ts
+++ client/src/components/icon/icon.ts
@@ -1,10 +1,10 @@
 import { Component, Input } from '@angular/core';
 import { MatIconRegistry } from '@angular/material/icon';
 import { DomSanitizer } from '@angular/platform-browser';
-import { environment } from 'environments/environment';
+import { environment } from '../../environments/environment';
 
-export type Icon = 'play_audio'|'record_audio'|'stop_audio'|'play_recording'|'arrow_back'|'back'|'add_feedback'|
-  'add_to_home'|'brand'|'photo_library'|'capture_photo'|'menu'|'close'|'share'|'search'|'add'|'language'|'forward'|
+export type Icon = 'play_audio' | 'record_audio' | 'stop_audio' | 'play_recording' | 'arrow_back' | 'back' | 'add_feedback' |
+  'add_to_home' | 'brand' | 'photo_library' | 'capture_photo' | 'menu' | 'close' | 'share' | 'search' | 'add' | 'language' | 'forward' |
   'read_more';
 
 @Component({
@@ -14,9 +14,9 @@ export type Icon = 'play_audio'|'record_audio'|'stop_audio'|'play_recording'|'ar
 })
 export class IconComponent {
   @Input()
-  public icon: Icon|null = null;
+  public icon: Icon | null = null;
   @Input()
-  public color: 'primary'|'accent'|'warn'|null = null;
+  public color: 'primary' | 'accent' | 'warn' | null = null;
   @Input()
   public shadow = false;
 
@@ -26,7 +26,7 @@ export class IconComponent {
 
   private registerIcons() {
     const baseUrl = environment.assets.baseUrl + 'assets/icons/';
-    const icons: {[index: string]: string} = {
+    const icons: { [index: string]: string } = {
       capture_photo: 'radio_button_unchecked.svg',
       menu: 'menu.svg',
       close: 'close.svg',
